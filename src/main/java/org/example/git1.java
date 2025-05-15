2d7160c182edabfee15f6e1aee25237928b162df
SYasmin464 <yasminshakhina@gmail.com>
2025-05-15 16:19:33 -0600

    2nd commit

--- src/main/java/org/example/git1.java
+++ src/main/java/org/example/git1.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class git1 {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+        System.out.println("life is beautiful");
     }
 }
