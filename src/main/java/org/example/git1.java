d5c3e515acde9fd710204dc7753e5055f883736e
SYasmin464 <yasminshakhina@gmail.com>
2025-05-15 16:47:14 -0600

    Update git1.java

--- src/main/java/org/example/git1.java
+++ src/main/java/org/example/git1.java
@@ -4,6 +4,7 @@ public class git1 {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-        System.out.println("life is beautiful");
+        System.out.println("life is sky");
+        
     }
 }
